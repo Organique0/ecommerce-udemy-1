5b332569a288b7651335fd0d07ac11a09ce569fe
Luka Grabnar <grabnar.luka@gmail.com>
2023-10-11 20:53:42 +0200

    firebase auth

--- app/utils/firebase/firebase.utils.js
+++ app/utils/firebase/firebase.utils.js
@@ -8,6 +8,17 @@ import {
   signInWithRedirect,
 } from "firebase/auth";
 
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC-BEonelC5inPLnODd6B9LLNSjilr2XFk",
   authDomain: "udemy-ecommerce-1.firebaseapp.com",
@@ -26,3 +37,27 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return userDocRef;
+};
