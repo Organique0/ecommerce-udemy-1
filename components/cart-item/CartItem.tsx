415bb15e11f992ee78616a7f36b283de3ac621ab
Organique0 <grabnar.luka@gmail.com>
2023-10-16 20:51:32 +0200

    cart items

--- components/cart-item/CartItem.tsx
+++ components/cart-item/CartItem.tsx
@@ -1,10 +1,15 @@
 import { ProductWithQuantity } from "@/contexts/cart.context";
+import Image from "next/image";
+import "./cart-items.styles.scss"
 
 const CartItem = ({ cartItem }: { cartItem: ProductWithQuantity }) => {
     return (
-        <div>
-            <h2>{cartItem.name}</h2>
-            <span>{cartItem.quantity}</span>
+        <div className="cart-item-container">
+            <Image src={cartItem.imageUrl} alt={`${cartItem.name}`} width={100} height={60} />
+            <div className="item-details">
+                <span className="name">{cartItem.name}</span>
+                <span className="price">{cartItem.quantity} X {cartItem.price}</span>
+            </div>
         </div>
     )
 }
