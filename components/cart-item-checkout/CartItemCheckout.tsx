cedb2b225f0222c302ed546fdd073622ed18b03c
Organique0 <grabnar.luka@gmail.com>
2023-10-17 15:56:48 +0200

    cart dropdown styles

--- components/cart-item-checkout/CartItemCheckout.tsx
+++ components/cart-item-checkout/CartItemCheckout.tsx
@@ -10,21 +10,21 @@ const CartItemCheckout = ({ product, add, remove, removeAll }: { product: Produc
     return (
         <div className="checkout-item-container">
             <div className="image-container">
-                <Image src={imageUrl} width={20} height={20} alt={`product-${id}`} />
+                <Image src={imageUrl} width={200} height={200} alt={`product-${id}`} />
             </div>
             <div className="name">
                 {name}
             </div>
             <div className="quantity">
-                <button className="arrow" onClick={() => remove(product)}>down</button>
+                <span className="arrow" onClick={() => remove(product)}>{`<`}</span>
                 <div className="value">{quantity}</div>
-                <button className="arrow" onClick={() => add(product)}>up</button>
+                <span className="arrow" onClick={() => add(product)}>{`>`}</span>
             </div>
             <div className="price">
                 {price}
             </div>
             <td>
-                <button type="button" onClick={() => removeAll(product)}>X</button>
+                <span className="remove-button" onClick={() => removeAll(product)}>X</span>
             </td>
         </div>
     )
