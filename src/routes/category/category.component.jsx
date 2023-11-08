266333104687a3f2a07caaba0841ca1c47aa91c7
Luka Grabnar <grabnar.luka@gmail.com>
2023-11-08 21:55:51 +0100

    with apollo

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,20 +1,60 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-
 import ProductCard from '../../components/product-card/product-card.component';
-
 import { CategoriesContext } from '../../contexts/categories.context';
-
 import { CategoryContainer, Title } from './category.styles';
 
+import { gql, useMutation, useQuery } from '@apollo/client';
+
+const GET_CATEGORY = gql`
+  query($title:String!) {
+    getCollectionsByTitle(title:$title){
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
+const SET_CATEGORY = gql`
+  mutation($category:Category!) {
+    addCategory(category:$category) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }}
+`
+
 const Category = () => {
+  const [products, setProducts] = useState([]);
+  
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+/*   const {loading, error, data} = useQuery(GET_CATEGORY,{
+    variables:{
+      title:category
+    }
+  }); */
+
+  const [addCategory, {loading, error, data}] = useMutation(SET_CATEGORY);
+
+  addCategory({variables:{category:category}});}
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if(data) {
+      const {getCollectionsByTitle:{items}} = data;
+      setProducts(items)
+    }
+  },[category, data]);
 
   return (
     <Fragment>
